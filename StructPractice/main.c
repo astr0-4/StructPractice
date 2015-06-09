8cbd1e55e0a299e72d0d9503821323d7aa1200cd
Alex Stroulger <alexstroulger@gmail.com>
2015-06-09 14:19:32 -0400

    Initial Commit

---
+++ StructPractice/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  StructPractice
+//
+//  Created by Alex on 2015-06-09.
+//  Copyright (c) 2015 Alex. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
