ff4e753869c249dfc70c86672cd2207bfe6eb61a
Alex Stroulger <alexstroulger@gmail.com>
2015-06-09 14:59:01 -0400

    Finished Struct exercise

--- StructPractice/main.c
+++ StructPractice/main.c
@@ -8,8 +8,62 @@
 
 #include <stdio.h>
 
+typedef struct {
+    float l;
+    float w;
+    float h;
+} Box;
+
+Box buildBox();
+float volume(Box myBox);
+Box boxMaker(float length, float width, float height);
+float boxComparer(Box box1, Box box2);
+Box buildAnotherBox();
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+    
+    Box box3 = buildBox();
+    Box box4 = buildAnotherBox();
+    
+    printf("%f",volume(box3));
+    boxMaker(box3.l, box3.w, box3.h);
+    float boxRatio = boxComparer(box4, box3);
+    printf("%f", boxRatio);
+}
+
+Box buildBox() {
+    Box box;
+    box.l = 10;
+    box.w = 11;
+    box.h = 12;
+    return box;
+}
+
+Box buildAnotherBox() {
+    Box box;
+    box.l = 20;
+    box.w = 22;
+    box.h = 24;
+    return box;
+}
+
+
+
+float volume(Box box) {
+    return box.l * box.w * box.h;
+}
+
+Box boxMaker(float length, float width, float height) {
+    Box box;
+    box.l = length;
+    box.w = width;
+    box.h = height;
+    return box;
+}
+
+float boxComparer(Box box1, Box box2) {
+    float box1Volume = volume(box1);
+    float box2Volume = volume(box2);
+    
+    return box1Volume / box2Volume;
 }
